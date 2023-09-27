fb7446ac92ca1fa3eb28144dcdd460aefe779117
amer-aljaberi <amerjaberi2002@gmail.com>
2023-09-27 11:21:28 +0300

    inital commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var username = prompt("Please enter your name:");
+alert("Welcome to the page, " + username);
+
+var age = prompt("How old are you?");
+alert("Thank You");
+
+if (age > 15) {
+    console.log("You're an adult");
+} else {
+    console.log("Sorry, you cannot log in to this webpage because you are under 15 years old.");
+}
+
+       
